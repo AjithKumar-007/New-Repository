509185d302c9f8bfd1d053f5b64ab8041792a55f
AjithKumar-007 <ajithkumar2626@gmail.com>
2023-01-12 12:23:05 +0530

    Successfully updated

--- Lms.java
+++
@@ -1,5 +0,0 @@
-package day6For1;
-
-public class Lms {
-
-}
--- Sample1.java
+++ Sample1.java
@@ -5,5 +5,7 @@ public static void main(String[]args) {
 	for(int i=0;i<5;i++){
 		System.out.println(i);	
 	}
+	String a="Ajith";
+	System.out.println(a);
 }
 }
